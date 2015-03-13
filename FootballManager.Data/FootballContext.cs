e5abb3456a2d7553788cca5aafc52261c5bd4945
Phuc Vo <duyphuc_forza@yahoo.com>
2015-03-13 19:31:48 +0700

    Commit new source

---
+++ FootballManager.Data/FootballContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using FootballManager.Data.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace FootballManager.Data
+{
+    public class FootballContext : DbContext
+    {
+        public FootballContext()
+            : base("FootballContext")
+        {
+
+        }
+
+        public DbSet<Team> Teams { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
