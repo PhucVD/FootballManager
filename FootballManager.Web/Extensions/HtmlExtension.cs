e5c812257dad7790178fe6466afeabf5d0e0aee2
Phuc Vo <duyphuc_forza@yahoo.com>
2016-06-13 18:24:55 +0700

    Add HtmlExtension for ajax button.

---
+++ FootballManager.Web/Extensions/HtmlExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+
+namespace FootballManager.Web.Extensions
+{
+    public static class HtmlExtension
+    {
+        public static MvcHtmlString AjaxButton(this HtmlHelper helper, string label, AjaxButtonOptions options, object htmlAttributes = null)
+        {
+            TagBuilder tabBuilder = new TagBuilder("button");
+            tabBuilder.MergeAttribute("type", "button");
+            tabBuilder.MergeAttribute("class", "ajax-button");
+            tabBuilder.AddCssClass(options.CssClass);
+
+            tabBuilder.MergeAttribute("data-url", options.Url);
+            tabBuilder.MergeAttribute("data-method", options.Method.ToUpper());
+            tabBuilder.MergeAttribute("data-target-id", options.TargetId);
+            tabBuilder.SetInnerText(label);
+            if (htmlAttributes != null)
+            {
+                var attributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+                tabBuilder.MergeAttributes(attributes);
+            }
+
+            return new MvcHtmlString(tabBuilder.ToString());
+        }
+    }
+
+    public class AjaxButtonOptions
+    {
+        public string Url { get; set; }
+        public string Method { get; set; }
+        public string TargetId { get; set; }
+        public string CssClass { get; set; }
+
+    }
+}
